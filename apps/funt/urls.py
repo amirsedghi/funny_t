67a8cf10a6fdf0ff068cd512c07aa1b068a574ea
amirsedghi <amir.sepultura@gmail.com>
2016-08-24 08:08:30 -0700

    done with product portion of the admin

--- apps/funt/urls.py
+++ apps/funt/urls.py
@@ -2,5 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^admin$', views.admin),
+    url(r'^login$', views.login),
+    url(r'^dashboard/orders/(?P<id>\d+)$', views.orderdash),
+    url(r'^dashboard/products/(?P<id>\d+)$', views.productdash),
+    url(r'^add$', views.add),
+    url(r'^product/adding$', views.adding),
+    url(r'^edit/(?P<id>\d+)$', views.edit),
+    url(r'^delete/(?P<id>\d+)$', views.delete),
+    url(r'^editing/(?P<id>\d+)$', views.editing),
 ]
